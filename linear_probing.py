b802aa54eab69b73f3ebd955f5ee875d603473b5
riverstone496 <robosatoki@outlook.jp>
2024-05-04 17:03:17 +0900

    fix lp

--- linear_probing.py
+++ linear_probing.py
@@ -672,6 +672,7 @@ if __name__=='__main__':
     parser.add_argument('--class_reduction_type', type=str, default='mean')
     parser.add_argument('--permutate', action='store_true', default=False)
     parser.add_argument('--train_classes', type=str, default=None)
+    parser.add_argument('--pretrained_classes', type=str, default=None)
     parser.add_argument('--task1_class', type=int, default=10)
     parser.add_argument('--task2_class', type=int, default=10)
 
@@ -689,10 +690,11 @@ if __name__=='__main__':
             config = json.load(f)
         dict_args.update(config)
     print(args)
-
     if args.train_classes is not None:
         args.train_classes = args.train_classes.split(',')
         args.train_classes = [int(c) for c in args.train_classes]
+        args.pretrained_classes = args.pretrained_classes.split(',')
+        args.pretrained_classes = [int(c) for c in args.pretrained_classes]
 
     args.cuda = not args.no_cuda and torch.cuda.is_available()
     job_id = os.environ.get('SLURM_JOBID')
@@ -728,7 +730,7 @@ if __name__=='__main__':
         pretrained_dataset = utils.dataset.FashionMNIST(args=args)
         dataset = utils.dataset.FashionMNIST(args=args)
     elif args.dataset == 'CIFAR10':
-        pretrained_dataset = utils.dataset.CIFAR10(args=args, task_classes=args.train_classes)
+        pretrained_dataset = utils.dataset.CIFAR10(args=args, task_classes=args.pretrained_classes)
         dataset = utils.dataset.CIFAR10(args=args, task_classes=args.train_classes)
 
     dataset_original_class = dataset.num_classes
